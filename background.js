2c30ade31385f3128e7790cb637fddfd60dde4f5
Utkarsh299-tech <singhutkarsh2799@gmail.com>
2021-07-05 19:58:18 +0530

    Added regex to change Mary to cat emoji in background.js

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 function poopifyRyan() {
   let re = /((R|r)yan)/g;
   let re2 = /((S|s)wift)/g;
+  let re3 = /((M|m)ary)/g;
   let element = document.querySelectorAll('p,span,div,li,h1,h2,h3,h4,h5,h6');
   for (let i = 0; i < element.length; i++) {
     element[i].innerHTML = element[i].innerHTML.replace(re, "💩")
-    element[i].innerHTML = element[i].innerHTML.replace(re2, "🦈")
+    element[i].innerHTML = element[i].innerHTML.replace(re2, "🦈 ")
+    element[i].innerHTML = element[i].innerHTML.replace(re3, "😻")
   }
 }
 
